811c5ad3479197035dd50905632526ddf0d58cf2
AndreiKorsunov <andrei.korshunov2004@gmail.com>
2022-09-07 09:14:01 +0300

    x

--- Loomad/koduloom.cs
+++ Loomad/koduloom.cs
@@ -8,16 +8,14 @@
         public sugu loomaSugu;
         public double kaal;
         public int vanus;
-        public bool elav; //Kui true, siis elav; kui false siis on surnud
         public koduloom() { }
-        public koduloom(string nimi, string varv, sugu loomaSugu, double kaal = 0.0, int vanus = 0, bool elav = false)
+        public koduloom(string nimi, string varv, sugu loomaSugu, double kaal = 0.0, int vanus = 0)
         {
             this.nimi = nimi;
             this.varv = varv;
             this.loomaSugu = loomaSugu;
             this.kaal = kaal;
             this.vanus = vanus;
-            this.elav = elav;
         }
         public abstract void print_Info();
         public abstract void print_Haal();
